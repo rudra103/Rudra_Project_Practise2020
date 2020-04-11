b9e61c162581fb6d81d26c8ca6fe105c59bb405b
rudra1233 <mymailcontact1233@gmail.com>
2020-04-11 19:31:10 +0530

    1modified,1newly added java file

--- src/snippet/sampleGitTest.java
+++ src/snippet/sampleGitTest.java
@@ -1,11 +1,11 @@
 package snippet;
 
-public class Snippet {
+public class sampleGitTest {
+
 	public static void main(String[] args) {
 
-		 
-		System.out.println("GIT test1");
+ 
 		System.out.println("GIT Testing..");
 	}
-}
 
+}
